609d312bd8463293a99cda07497d28502b6106bd
Serhii Kupriienko <skupriienko@anaconda.com>
2023-04-21 18:03:05 +0300

    Fix scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,9 @@
+set NO_DIST=1
+
+set DAAL4PY_VERSION=%PKG_VERSION%
+set MPIROOT=%PREFIX%\Library
 IF DEFINED DAALROOT (set DALROOT=%DAALROOT%)
 IF NOT DEFINED DALROOT (set DALROOT=%PREFIX%)
+set PATH=%PREFIX%\Library\bin;%PATH%;%PREFIX%\Library\bin\libfabric
 
 %PYTHON% setup.py install --single-version-externally-managed --record record.txt
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if [ "$PY3K" == "1" ]; then
+    ARGS="--single-version-externally-managed --record=record.txt"
+else
+    ARGS="--old-and-unmanageable"
+fi
+
+# if dpc++ vars path is specified
+if [ ! -z "${DPCPPROOT}" ]; then
+    source ${DPCPPROOT}/env/vars.sh
+    export CC=dpcpp
+    export CXX=dpcpp
+    dpcpp --version
+fi
+
 # if DAALROOT not exists then provide PREFIX
 if [ "${DAALROOT}" != "" ] && [ "${DALROOT}" == "" ] ; then
     export DALROOT="${DAALROOT}"
@@ -9,4 +23,10 @@ if [ -z "${DALROOT}" ]; then
     export DALROOT=${PREFIX}
 fi
 
-"$PYTHON" setup_sklearnex.py install --single-version-externally-managed --record=record.txt
+if [ "$(uname)" == "Darwin" ]; then
+    export CXX=clang++
+fi
+
+export DAAL4PY_VERSION=$PKG_VERSION
+export MPIROOT=${PREFIX}
+${PYTHON} setup.py install $ARGS
