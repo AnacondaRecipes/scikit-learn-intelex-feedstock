e248e21ac4e0a07554a84614926115637cc65571
Serhii Kupriienko <skupriienko@anaconda.com>
2023-04-25 14:37:58 +0300

    Simplify script and use script_env, update dal version template

--- recipe/bld.bat
+++
@@ -1,3 +0,0 @@
-IF NOT DEFINED DALROOT (set DALROOT=%PREFIX%)
-
-%PYTHON% setup_sklearnex.py install --single-version-externally-managed --record=record_sklearnex.txt
--- recipe/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export DALROOT=${PREFIX}
-
-${PYTHON} setup_sklearnex.py install --single-version-externally-managed --record=record_sklearnex.txt
